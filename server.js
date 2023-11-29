71f3ce7cf3f30feefb8ba803f16ac6ad5f45acac
shankar405 <shankarmalviya067@gmail.com>
2023-11-29 11:37:44 +0530

    connect to mongo atlas

--- server.js
+++ server.js
@@ -1,33 +1,52 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const dotenv = require('dotenv').config({ PATH: './env' })
+const connectDB = require("./db/index.js");
+connectDB()
+// const express = require("express");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+
+
+
+
+
+
 
 // MongoDB connection (replace with your MongoDB URI) mongodb://127.0.0.1:27017/
-const uri =
-  "mongodb+srv://shankarmalviya067:Shankar_067@cluster0.n7dvmwe.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const uri =
+//   "mongodb+srv://shankarmalviya067:Shankar_067@cluster0.n7dvmwe.mongodb.net/?retryWrites=true&w=majority";
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to MongoDB");
+// });
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello, MERN Chat App!");
-});
+// // Basic route
+// app.get("/", (req, res) => {
+//   res.send("Hello, MERN Chat App!");
+// });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//   } catch (error) {
+//     console.log("ERROR:", error)
+//     throw err
+//   }
+// })()
